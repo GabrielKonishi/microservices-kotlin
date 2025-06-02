d79f5c4e285c0a8e8287bea6cf998cf5567962c0
gabriel <gabriel.ryuji.konishi@gmail.com>
2025-06-02 20:51:23 +0900

    Rest Api project created using spring intlz

---
+++ rest-with-spring-boot-and-kotlin/rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/study/restApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiApplication>(*args)
+}
